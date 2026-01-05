d81c8d9f78ee29d4e89a5a9ff98cfa784b1b4efb
Ahmad Shahrour <ashahrourr@gmail.com>
2026-01-05 13:36:30 +0300

    x

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = {
   title: "TalktoAnatomy",
   description: "3D Anatomy Viewer",
   icons: {
-    icon: "/icon.png"
+    icon: "/icon.png",
   },
 };
 
@@ -22,6 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token":"af75fc1b51964fe4ba02817d82d635f9"}'
+        />
+      </head>
+
       <body className="min-h-[100svh] bg-[#1c1c1c] text-white">
         {children}
       </body>
